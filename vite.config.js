c5f39365208677d4527cd143ee575060b1d040ea
Nielan2008 <nap.ukdh@gmail.com>
2026-05-29 10:29:43 +0200

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    host: '0.0.0.0'
+    allowedHosts: ['eight-1-u-festival-app-2026.onrender.com']
   }
-});
+})
